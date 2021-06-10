7a8315853188a4229869de87828b1c01589cab89
Mangonando <juan.f.ramirezv@gmail.com>
2021-06-10 12:35:21 +0200

    unfinished lab

--- js/index.js
+++ js/index.js
@@ -44,7 +44,24 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      card.classList.toggle('turned')
+    let cardTimeOut = setTimeout( function() {
+      card.classList.toggle('turned')
+    }, 3000)
+    memoryGame.checkIfPair
+      // 1. flip 2. compare with the other flipped 3. if they 
+      // are the same stay 4. if not they should flip back after 3 seg
+      //  / define own array and push every card into this array if it 
+      // has a length of 2 check pair in these two cards
+// 1) add the class 'turned'. You need to make sure only two cards are 
+// turned at any given time. So add an if-condition to check if the number 
+// of pickedCards if it is less than 2 before turning any card.
+// 2) Push the turned card into the memory game pairsClicked array.
+// 3) Add an if-condition to check it pairsClicked array has a length of 2
+//  --> this means there are two cards that are clicked and turned.
+// 4) If the above condition is true, use checkIfPair on these 2 cards to check if they pair.
+// 5) If they pair, update the number of pairsGuessed and check if the game is finished.
+// 6) If they don't, set a timer to flip the cards back, remove the 2 cards from the pickedCards array
       console.log(`Card clicked: ${card}`);
     });
   });
--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,40 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    let currentIndex = this.cards.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
+    return this.cards
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1; 
+      return true
+    }
+    return false
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === this.cards.length / 2) {
+      console.log(`You finished, well done 🎉`)
+      return true
+    } else {
+      console.log(`Game must go on 🧐`)
+      return false
+    }
   }
 }
 
